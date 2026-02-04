79c09913dbf609f8e361dfb5187ff9bbd999fdf9
Jason Yoder <jason.yoder2@bsu.edu>
2026-02-04 15:51:55 -0500

    Fix package names

--- backend/src/main/java/com/mergeflow/backend/controller/HealthController.java
+++ backend/src/main/java/com/mergeflow/backend/controller/HealthController.java
@@ -1,13 +1,15 @@
-package com.flowstate.backend.controller;
+package com.mergeflow.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/health")
+@CrossOrigin(origins = "http://localhost:3000")
 public class HealthController {
 
     @GetMapping
